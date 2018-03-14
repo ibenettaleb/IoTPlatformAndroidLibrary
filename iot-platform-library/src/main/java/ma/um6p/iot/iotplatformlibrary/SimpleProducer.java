ba59b7c92eb34725a28d241016e558c65005b6b7
ibenettaleb@gmail.com <abdeljalil.benettaleb@um6p.ma>
2018-03-14 12:10:21 +0000

    Use Producer Class an Android App

--- iot-platform-library/src/main/java/ma/um6p/iot/iotplatformlibrary/SimpleProducer.java
+++ iot-platform-library/src/main/java/ma/um6p/iot/iotplatformlibrary/SimpleProducer.java
@@ -11,10 +11,9 @@ import java.util.Properties;
 public class SimpleProducer {
     public static void main(String[] args) throws Exception {
         System.out.println("Hi From SimpleProducer");
-        createTopic("Topic","Key", "Value");
     }
 
-    public static void createTopic(String topicName, String key, String value) {
+    private static void createTopic(String topicName, String key, String value) {
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092,localhost:9093");
         props.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
